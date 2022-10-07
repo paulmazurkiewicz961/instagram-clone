6ec49890179c52452d92819c6cd552d28f366db5
paulmazurkiewicz961 <83549565+paulmazurkiewicz961@users.noreply.github.com>
2022-10-07 16:10:51 +0200

    Completed footer

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,96 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    
+      
+    <q-footer class="bg-white text-grey"  >
+      <q-toolbar>
+        <q-btn label="Meta" flat/> 
+        <q-btn label="About" flat/> 
+        <q-btn label="Blogs" flat/> 
+        <q-btn label="Jobs" flat/> 
+        <q-btn label="Help" flat/> 
+        <q-btn label="API" flat/> 
+        <q-btn label="Privacy" flat/> 
+        <q-btn label="Terms" flat/> 
+        <q-btn label="Top Accounts" flat/> 
+        <q-btn label="Hashtags" flat/> 
+        <q-btn label="Locations" flat/> 
+        <q-btn label="Instagram Lite" flat/> 
+        <q-btn label="Contact uploading and non-users" flat/> 
+        <q-btn label="Dance" flat/> 
+        <q-btn label="Food and drinks" flat/> 
+        <q-btn label="Home and garden" flat/> 
+        <q-btn label="Music" flat/> 
+        
+          <!-- To-do: Complete Buttons -->
+      </q-toolbar>
+    </q-footer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page
+    class="window-height window-width row justify-center items-center"
+    style="background: linear-gradient(#0000);"
+  >
+    <div class="column q-pa-lg">
+      <div class="row">
+        <q-img src="../assets/iphone.png" /> 
+      </div>
+    </div>
+    <div class="column q-pa-lg">
+      <div class="row">
+        <q-card square class="shadow-24" style="width:300px;height:485px;">
+          <q-card-section class="bg-deep-purple-7">
+            <h4 class="text-h5 text-white q-my-md">Instagram</h4>
+            <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
+              
+            </div>
+          </q-card-section>
+          <q-card-section>
+            <q-form class="q-px-sm q-pt-xl q-pb-lg">
+              <q-input square clearable v-model="email" type="email" label="Email or username">
+                <template v-slot:prepend>
+                  <q-icon name="email" />
+                </template>
+              </q-input>
+              <q-input square clearable v-model="password" type="password" label="Password">
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+              </q-input>
+            </q-form>
+          </q-card-section>
+          <q-card-actions class="q-px-lg">
+            <q-btn unelevated size="lg" color="purple-4" class="full-width text-white" label="Log In" />
+          </q-card-actions>
+          <q-card-section class="text-center q-pa-sm">
+            <p class="text-grey-6">Forgotten your password?</p>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
