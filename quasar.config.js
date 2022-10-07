6110eb51e64961e8166612da952ba5b48812bbc5
Anthony Bergen <anthonydbergen@gmail.com>
2022-10-07 10:30:02 -0400

    fixed rendering of phone image

--- quasar.config.js
+++ quasar.config.js
@@ -56,7 +56,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
+        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16'
       },
 
@@ -93,6 +93,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
+      port: 8080,
       open: true // opens browser window automatically
     },
 
@@ -133,7 +134,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
       // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-                                          // will mess up SSR
+      // will mess up SSR
 
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
@@ -144,7 +145,7 @@ module.exports = configure(function (/* ctx */) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
         'render' // keep this as last one
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,30 +1,42 @@
 <template>
   <q-page
     class="window-height window-width row justify-center items-center"
-    style="background: linear-gradient(#0000);"
+    style="background: linear-gradient(#0000)"
   >
-    <div class="column q-pa-lg">
-      <div class="row">
-        <q-img src="../assets/iphone.png" /> 
+    <div class="row">
+      <div class="col-6 q-px-xl">
+        <q-img
+          height="500px"
+          width="300px"
+          src="../assets/iphone.png"
+          class="q-mr-xl"
+        />
       </div>
-    </div>
-    <div class="column q-pa-lg">
-      <div class="row">
-        <q-card square class="shadow-24" style="width:300px;height:485px;">
+      <div class="col-6">
+        <q-card square class="shadow-24" style="width: 300px; height: 485px">
           <q-card-section class="bg-deep-purple-7">
-            <h4 class="text-h5 text-white q-my-md">Instagram</h4>
-            <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
-              
-            </div>
+            <div class="text-h5 text-white q-my-md">Instagram</div>
           </q-card-section>
           <q-card-section>
             <q-form class="q-px-sm q-pt-xl q-pb-lg">
-              <q-input square clearable v-model="email" type="email" label="Email or username">
+              <q-input
+                square
+                clearable
+                v-model="user.email"
+                type="email"
+                label="Email or username"
+              >
                 <template v-slot:prepend>
                   <q-icon name="email" />
                 </template>
               </q-input>
-              <q-input square clearable v-model="password" type="password" label="Password">
+              <q-input
+                square
+                clearable
+                v-model="user.password"
+                type="password"
+                label="Password"
+              >
                 <template v-slot:prepend>
                   <q-icon name="lock" />
                 </template>
@@ -32,7 +44,13 @@
             </q-form>
           </q-card-section>
           <q-card-actions class="q-px-lg">
-            <q-btn unelevated size="lg" color="purple-4" class="full-width text-white" label="Log In" />
+            <q-btn
+              unelevated
+              size="lg"
+              color="purple-4"
+              class="full-width text-white"
+              label="Log In"
+            />
           </q-card-actions>
           <q-card-section class="text-center q-pa-sm">
             <p class="text-grey-6">Forgotten your password?</p>
@@ -43,9 +61,19 @@
   </q-page>
 </template>
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  setup() {
+    const user = reactive({
+      email: "",
+      password: "",
+    });
+
+    return {
+      user,
+    };
+  },
+});
 </script>
