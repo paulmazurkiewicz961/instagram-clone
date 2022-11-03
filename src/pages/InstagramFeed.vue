3046c1dafa21433287857863acf84986b977ea15
paulmazurkiewicz961 <83549565+paulmazurkiewicz961@users.noreply.github.com>
2022-11-03 18:20:27 +0100

    added drawer as well as buttons with icons

--- src/pages/InstagramFeed.vue
+++ src/pages/InstagramFeed.vue
@@ -1,6 +1,30 @@
 <script setup></script>
 <template>
-  <q-page class="window-height window-width row justify-center items-center">
-   
-  </q-page>
+<q-layout view="hHh lpR fFf">
+
+    <q-drawer 
+    show-if-above 
+    v-model="leftDrawerOpen" 
+    side="left" 
+    bordered
+    >
+    <div class="q-pt-md q-pl-md">
+    <q-img src="../assets/Instalogo.png" height="37px" width="150px" />
+    </div>
+    <div class="column q-pa-md q-gutter-sm" style="height: 530px">
+      <q-btn class="col" flat rounded align="left" icon="home" label="Home"/>
+      <q-btn class="col" flat rounded align="left" icon="search" label="Search"/>
+      <q-btn class="col" flat rounded align="left" icon="explore" label="Explore"/>
+      <q-btn class="col" flat rounded align="left" icon="send" label="Messages"/>
+      <q-btn class="col" flat rounded align="left" icon="favorite_border" label="Notifications"/>
+      <q-btn class="col" flat rounded align="left" icon="add_box" label="Create"/>
+      <q-btn class="col" flat rounded align="left" icon="account_circle" label="Profile"/>
+    </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
 </template>
